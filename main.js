6dbfaf2f39b6ac56b956f7bbb4f3e3848fb5afe4
Petra Spirkova <pspirkova@gmail.com>
2018-05-27 16:49:32 +0200

    add commmands

--- main.js
+++ main.js
@@ -8,7 +8,26 @@ function main() {
 
     function readUserInput() {
         rl.question("What are you up to?\n", (command) => {
-            console.log(command);
+            let action = command.split(" ")[1];
+            switch (action) {
+                case "->":
+                    console.log("posting​");
+                    break;
+                case undefined:
+                    console.log("reading");
+                    break;
+                case "follows":
+                    console.log("following");
+                    break;
+                case "wall":
+                    console.log("wall");
+                    break;
+                case "exit":
+                    console.log("exiting");
+                    return rl.close();
+                default:
+                console.log("i don't understand");
+            }
             readUserInput();
         });
     };
