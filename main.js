594f5dc9f09ec13907bc9593bd31518c1bc171bb
Petra Spirkova <pspirkova@gmail.com>
2018-05-27 16:32:19 +0200

    add readline, read user input

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+function main() {
+    // communicate with console
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout 
+    });
+
+    function readUserInput() {
+        rl.question("What are you up to?\n", (command) => {
+            console.log(command);
+            readUserInput();
+        });
+    };
+    readUserInput();
+}
+
+main();
+
