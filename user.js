3acd343d00bb97c91502da341827c2d36feead85
Petra Spirkova <pspirkova@gmail.com>
2018-05-31 10:08:19 +0200

    delete a  not needed file, change printMsg function

--- user.js
+++ user.js
@@ -19,13 +19,13 @@ class User {
             strName = this.name.toString() + ' - ';
         }
 
-        for (let i = 0; i < this.tweets.length; i++) {
+        for (let i = this.tweets.length-1; i>=0; i--) {
             console.log(strName + (this.tweets[i].toString()));
         }
     }
 
     addFollower(followName) {
-        this.follows.push(followname);
+        this.follows.push(followName);
     }
 }
 
