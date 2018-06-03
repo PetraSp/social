61a1e46a54906abb4c3005e67a5c39d2d80ba8c4
Petra Spirkova <pspirkova@gmail.com>
2018-06-03 08:14:27 +0200

    add calculateTimeElapsed function

--- tweet.js
+++ tweet.js
@@ -1,11 +1,26 @@
 class Tweet {
     constructor(text){
+        // whenever i write a tweet i have to specify this two things
+        // you can specify
         this.text = text;
         this.date = Date.now();
     }
 
     toString() {
-      return `${this.text} (${new Date(Date.now() - this.date).getMinutes()} minutes ago)`;
+        return `${this.text} (${this.calculateTimeElapsed()})`;
+    }
+
+    calculateTimeElapsed() {
+        //getSeconds while getMinutes are zero
+        while (new Date(Date.now() - this.date).getMinutes() <= 0){
+            return (new Date(Date.now() - this.date).getSeconds() + ' seconds ago');
+        }
+        
+        let strTime = 'minutes'
+        if (new Date(Date.now() - this.date).getMinutes() == 1){
+            strTime = 'minute';
+        }
+        return (new Date(Date.now() - this.date).getMinutes()) + ' ' + strTime + ' ago';
     }
 }
 
