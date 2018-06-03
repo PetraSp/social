d3b78a29e97d950ad5adf608783f52012bb701ca
Petra Spirkova <pspirkova@gmail.com>
2018-06-03 17:07:59 +0200

    modify addTweet, printWall, add comparison

--- user.js
+++ user.js
@@ -6,22 +6,55 @@ class User {
         this.tweets = [];
         this.follows = [];
     }
+
     addTweet(text) {
-        const newTweet = new Tweet(text);
+        const newTweet = new Tweet(this.name, text);
         this.tweets.push(newTweet);  
         return newTweet;
     }
 
-    printMessages(wall) {
-        let strName = "";
+    printMessages() {
+        for (let i = this.tweets.length-1; i >= 0; i--) {
+            console.log(this.tweets[i].toString());
+        }
+    }
 
-        if (wall) {
-            strName = this.name.toString() + ' - ';
+    printWall() {
+        // let strName = "";
+
+        // if {
+        //     strName = this.name.toString() + ' - ';
+        // }
+
+        // for (let i = this.tweets.length-1; i>=0; i--) {
+        //     console.log(strName + (this.tweets[i].toString()));
+        // }
+        const wallTweets = this.tweets;
+
+        for (let i = 0; i < this.follows.length; i++) {
+            const followedTweets = this.follows[i].getTweets();
+            wallTweets.push(...followedTweets);
         }
 
-        for (let i = this.tweets.length-1; i>=0; i--) {
-            console.log(strName + (this.tweets[i].toString()));
+        wallTweets.sort(this.compareTweets);
+        
+        for (let i = 0; i < wallTweets.length; i++) {
+            console.log(`${wallTweets[i].getAuthor()} - ${wallTweets[i].toString()}`);
         }
+    }
+
+    compareTweets(a, b) {      
+        let comparison = 0;
+        if (a.getDate() > b.getDate()) {
+          comparison = -1;
+        } else if (a.getDate() < b.getDate()) {
+          comparison = 1;
+        }
+        return comparison;
+    }
+
+    getTweets() {
+        return this.tweets;
     }
 
     addFollower(followName) {
